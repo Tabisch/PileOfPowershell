e676aed69c082e743715b4dc23bfa3263330b630
Tabisch <tobithieman@gmail.com>
2021-12-15 10:31:51 +0100

    Add files via upload

---
+++ RemoveFoldersWithoutFiles.ps1
@@ -0,0 +1,18 @@
+﻿Param(
+$Path
+)
+
+#Removes all folders not containing files
+
+while($true)
+{
+    $LeereOrdner = Get-ChildItem -Path $Path -Directory -Recurse | Where-Object{$_.PSIsContainer -eq $True} | Where-Object{$_.GetFileSystemInfos().Count -eq 0}
+
+    if($LeereOrdner.Length -eq 0)
+    {
+        Write-Host Removed all empty Folders
+        break
+    }
+
+    $LeereOrdner  | Remove-Item
+}
