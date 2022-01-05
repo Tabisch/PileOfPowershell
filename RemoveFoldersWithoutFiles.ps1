49d5f579cc0314d0bcb8c48881635e4221226643
Tabisch <tobithieman@gmail.com>
2022-01-05 16:09:53 +0100

    Update RemoveFoldersWithoutFiles.ps1

--- RemoveFoldersWithoutFiles.ps1
+++ RemoveFoldersWithoutFiles.ps1
@@ -3,16 +3,17 @@ $Path
 )
 
 #Removes all folders not containing files
+#Loops until every Folder Contains atleast one file
 
 while($true)
 {
-    $LeereOrdner = Get-ChildItem -Path $Path -Directory -Recurse | Where-Object{$_.PSIsContainer -eq $True} | Where-Object{$_.GetFileSystemInfos().Count -eq 0}
+    $emptyFolders = Get-ChildItem -Path $Path -Directory -Recurse | Where-Object{$_.PSIsContainer -eq $True} | Where-Object{$_.GetFileSystemInfos().Count -eq 0}
 
-    if($LeereOrdner.Length -eq 0)
+    if($emptyFolders.Length -eq 0)
     {
-        Write-Host Removed all empty Folders
+        Write-Host "Removed all empty Folders"
         break
     }
 
-    $LeereOrdner  | Remove-Item
+    $emptyFolders  | Remove-Item
 }
