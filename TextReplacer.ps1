381c5d5d296addad2fe7746028d84a94b03eb7bb
Tabisch <tobithieman@gmail.com>
2022-01-05 16:06:38 +0100

    Add files via upload

---
+++ TextReplacer.ps1
@@ -0,0 +1,39 @@
+﻿Function Replace-Text(){
+Param(
+[String]$Pfad
+)
+
+$fileen = Get-ChildItem -Path $Pfad -Recurse -Filter "*.xls"
+
+Write-host "Get-ChildItem completet"
+
+foreach($file in $files)
+{
+    Write-Host "Loading $($file.FullName)"
+
+    $lastwrite = $file.LastWriteTime
+     
+    $FileContent = Get-Content -Path $file.FullName
+
+    $FileContent = $FileContent.Replace("OldText","NewText")
+
+    Write-Host "Setting Content for $($file.FullName)"
+
+    $FileContent | Set-Content -Path $file.FullName
+
+    $file.LastWriteTime = $lastwrite
+}
+
+}
+
+$paths = @(
+"",
+""
+)
+
+
+foreach($path in $paths)
+{
+    Write-host $path
+    Replace-Text -Pfad $path
+}
