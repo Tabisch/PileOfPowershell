9e7a321b2c0218ab489e2ed072ee42bfe2b95b85
Tabisch <tobithieman@gmail.com>
2022-02-04 13:23:59 +0100

    Add files via upload

--- FileWatcher.ps1
+++ FileWatcher.ps1
@@ -5,7 +5,9 @@ $MailFrom,
 $MailTo,
 $MailServer = "outlook.bauer-gmbh.org",
 [parameter(Mandatory=$true)]
-$Path
+$Path,
+$HeadLineText = "Neue Dateien in Ordner `"$($Path)`"<br><br>",
+$MailSubject = "Es wurden neue Aufträge erstellt"
 )
 
 $PreviousFolderStateFile = "$($PSScriptRoot)\$($Path -replace '[^a-zA-Z0-9]', '').txt"
@@ -30,8 +32,8 @@ foreach($file in $allFiles)
 
 if($MailMessageBody -ne "")
 {
-    $MailMessageBody = "Neue Dateien in Ordner `"$($Path)`"<br><br>" + $MailMessageBody
-    Send-MailMessage -From $MailFrom -To $MailTo -Subject "Es wurden neue Aufträge erstellt" -SmtpServer $MailServer -Body $MailMessageBody -BodyAsHtml -Encoding utf8
+    $MailMessageBody = $HeadLineText + $MailMessageBody
+    Send-MailMessage -From $MailFrom -To $MailTo -Subject $MailSubject -SmtpServer $MailServer -Body $MailMessageBody -BodyAsHtml -Encoding utf8
 }
 
 $allFiles.FullName | Out-File -FilePath $PreviousFolderStateFile -Encoding utf8
