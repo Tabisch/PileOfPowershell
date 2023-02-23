30c58a3f8d01354d94d28cc468c0d44f0de59c67
Tabisch <tobithieman@gmail.com>
2023-02-23 14:02:18 +0100

    Add files via upload

---
+++ Find-Service.ps1
@@ -0,0 +1,21 @@
+﻿param(
+$Port,
+$ProcessId
+)
+
+if(!$ProcessId -and !$Port)
+{
+    Write-Host "No Port or ProcessId given"
+}
+
+if($ProcessId -and $Port)
+{
+    Write-Host "Port and ProcessId are mutually exlusive"
+}
+
+if($Port)
+{
+    $ProcessId = (Get-NetTCPConnection -LocalPort $Port).OwningProcess
+}
+
+(Get-CimInstance WIN32_SERVICE | Where-Object{ $_.ProcessId -eq $ProcessId }).DisplayName
